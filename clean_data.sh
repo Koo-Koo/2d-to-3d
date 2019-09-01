5227361f60f45b77c6e90f3aa6732f8c46e35688
Ubuntu <ubuntu@ip-172-31-45-175.us-east-2.compute.internal>
2019-09-01 11:00:16 +0000

    minor update and upload a bash file

---
+++ clean_data.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+rm openpose/sample_images/*
+rm openpose/sample_jsons/*
+rm openpose/sample_videos/*
+rm hmr/output/bvh_animation/*
+rm hmr/output/csv/*
+rm hmr/output/csv_joined/*
+rm hmr/output/images/*
