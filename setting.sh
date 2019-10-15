c0d9dd209ff71779bee62d08f0ec5f46ad2c10a0
Eujeanne Kim <41776587+puzzlepcs@users.noreply.github.com>
2019-10-15 17:33:43 +0900

    Create setting.sh

---
+++ setting.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+mkdir cali
+mkdir cali/center
+mkdir openpose/sample_videos
+mkdir openpose/sample_jsons
+mkdir openpose/sample_images
+mkdir hmr/output
+mkdir hmr/output/bvh_animation
+mkdir hmr/output/csv
+mkdir hmr/csv_joined
